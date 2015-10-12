c574bc3dfee57ae8a1380d2cf28500fdbc86f838
danielhollcraft <daniel@hollcraft.com>
2015-10-12 18:36:57 +0200

    Projectiles have maximum range property using a new class called NCUtils. All tests pass.

---
+++ Assets/Tests/Unit Tests/Editor/NCUtilsTests.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+
+public class NCUtilsTests {
+
+	[Test]
+	[Category("MaxRangeTravelled")]
+	public void IfMaxRangeTravelledReturnTrue() 
+	{	
+		bool result = NCUtils.CheckIfMaxRangeTravelled(10, Vector3.zero, new Vector3(10, 0, 0));
+		Assert.True(result);
+	}
+
+	[Test]
+	[Category("MaxRangeTravelled")]
+	public void IfMaxRangeNotTravelledReturnFalse() 
+	{	
+		bool result = NCUtils.CheckIfMaxRangeTravelled(11, Vector3.zero, new Vector3(10, 0, 0));
+		Assert.False(result);
+	}
+}
