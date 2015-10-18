c045c1ebedde1026f225ab03170db3fee2089a6f
danielhollcraft <daniel@hollcraft.com>
2015-10-18 15:27:50 +0200

    Working on Integration testing for wizard shield.

---
+++ Assets/Tests/Integration Tests/TestingAssets/ProjectileTestScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ProjectileTestScript : MonoBehaviour {
+
+	public Vector3 vector;
+	public Projectile projectile;
+
+	// Use this for initialization
+	void Start () {
+		projectile.Shoot(vector);
+	}
+
+}
