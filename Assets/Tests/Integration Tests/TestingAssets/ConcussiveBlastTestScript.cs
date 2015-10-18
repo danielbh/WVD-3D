23e81d935e54a4e92a732f5e6021af0025e9054c
danielhollcraft <daniel@hollcraft.com>
2015-10-18 19:48:56 +0200

    Tests that enemy is thrown back as expected by concussive blast.

---
+++ Assets/Tests/Integration Tests/TestingAssets/ConcussiveBlastTestScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConcussiveBlastTestScript : MonoBehaviour {
+	
+	void Start () {
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		player.gameObject.GetComponent<PlayerMagic>().CastConcussiveBlastSpell();
+	}
+}
