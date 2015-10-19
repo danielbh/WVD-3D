c5c4f5384106695366bce60b27c4cf1d608e5e27
danielhollcraft <daniel@hollcraft.com>
2015-10-19 20:06:58 +0200

    Beginning to work on Ice Nova. Now using visual studio instead of monodevelop.

--- Assets/Scripts/Characters/Player/Player.cs
+++ Assets/Scripts/Characters/Player/Player.cs
@@ -12,7 +12,7 @@ public class Player : MonoBehaviour {
 	const string CONCUSSIVE_BLAST = "CONCUSSIVE_BLAST_SPELL";
 	const string WIZARD_SHIELD = "WIZARD_SHIELD_SPELL";
 	const string TELEPORT = "TELEPORT_SPELL";
-	const string FROST_NOVA = "FROST_NOVA_SPELL";
+	const string ICE_NOVA = "ICE_NOVA_SPELL";
 	const string METEOR_SHOWER = "METEOR_SHOWER_SPELL";
 	
 	public float	aimStickDeadZone	= 0.2f;
@@ -127,8 +127,8 @@ public class Player : MonoBehaviour {
 				case TELEPORT:
 					magic.CastTeleportSpell();
 					break;
-				case FROST_NOVA:
-					magic.CastFrostNovaSpell();
+				case ICE_NOVA:
+					magic.CastIceNovaSpell();
 					break;
 				case METEOR_SHOWER:
 					magic.CastMeteorShowerSpell();
