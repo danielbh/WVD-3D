6c5ed9064d30ab4ebd896502e710a720176360f3
danielhollcraft <daniel@hollcraft.com>
2015-10-19 18:51:51 +0200

    Upgraded project to Unity 5.2. Adds tests for Concussive blast which is fully implemented as a spell prototype. All tests pass.

---
+++ Assets/Scripts/NCMonoBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class NCMonoBehaviour : MonoBehaviour {
+
+	protected IEnumerator InvokeMethod(Action method, float interval, int invokeCount)
+	{
+		for (int i = 0; i < invokeCount; i++)
+		{
+			method();
+			//print (i);
+			yield return new WaitForSeconds(interval);
+		}
+	}
+}
--- Assets/Tests/Integration Tests/TestingAssets/ConcussiveBlastTestScript.cs
+++ Assets/Tests/Integration Tests/TestingAssets/ConcussiveBlastTestScript.cs
@@ -1,10 +1,22 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using System;
 
-public class ConcussiveBlastTestScript : MonoBehaviour {
-	
-	void Start () {
-		GameObject player = GameObject.FindGameObjectWithTag("Player");
+public class ConcussiveBlastTestScript : NCMonoBehaviour 
+{
+
+	GameObject player;
+	public float castInterval = 0.5f;
+	public int timesCast = 3;
+
+	void Start () 
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		StartCoroutine(InvokeMethod(Cast, castInterval, timesCast));
+	}
+
+	void Cast()
+	{
 		player.gameObject.GetComponent<PlayerMagic>().CastConcussiveBlastSpell();
 	}
 }
