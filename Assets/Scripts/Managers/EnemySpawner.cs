75414487569681db9e02e29484747693e05347c4
danielhollcraft <daniel@hollcraft.com>
2015-11-13 15:20:55 +0100

    Hacks enemy spawners class to get unlimited waves of ALOT of enemies.

--- Assets/Scripts/Managers/EnemySpawner.cs
+++ Assets/Scripts/Managers/EnemySpawner.cs
@@ -17,11 +17,38 @@ public class EnemySpawner : MonoBehaviour {
 		
 		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
 		
-		if (enemies.Length == 0 && wave != 4) {
-			Invoke ("Wave" + wave, 0);
-			Debug.Log ("Wave " + wave + " called!");
-			++wave;
-		}
+		//if (enemies.Length == 0 && wave != 4) {
+		//	Invoke ("Wave" + wave, 0);
+		//	Debug.Log ("Wave " + wave + " called!");
+		//	++wave;
+
+            // HACK: Trying to just get a lot of enemies
+            //if (wave == 4)
+            //{
+
+        if (enemies.Length == 0)
+        {
+            SpawnDemons(new GameObject[] { melee1, ranged1, melee1 }, new int[] { 0, 2, 4 });
+            StartCoroutine(SpawnNextSet(3, new GameObject[] { melee1, ranged1, ranged1 }, new int[] { 1, 3, 5 }));
+
+            SpawnDemons(new GameObject[] { melee1, ranged1, melee1 }, new int[] { 0, 2, 4 });
+            StartCoroutine(SpawnNextSet(3, new GameObject[] { melee1, ranged1, ranged1 }, new int[] { 1, 3, 5 }));
+
+            SpawnDemons(new GameObject[] { melee1, ranged1, melee1 }, new int[] { 0, 2, 4 });
+            StartCoroutine(SpawnNextSet(3, new GameObject[] { melee1, ranged1, ranged1 }, new int[] { 1, 3, 5 }));
+
+            SpawnDemons(new GameObject[] { melee1, ranged1, melee1 }, new int[] { 0, 2, 4 });
+            StartCoroutine(SpawnNextSet(3, new GameObject[] { melee1, ranged1, ranged1 }, new int[] { 1, 3, 5 }));
+
+            SpawnDemons(new GameObject[] { melee1, ranged1, melee1 }, new int[] { 0, 2, 4 });
+            StartCoroutine(SpawnNextSet(3, new GameObject[] { melee1, ranged1, ranged1 }, new int[] { 1, 3, 5 }));
+
+            SpawnDemons(new GameObject[] { melee1, ranged1, melee1 }, new int[] { 0, 2, 4 });
+            StartCoroutine(SpawnNextSet(3, new GameObject[] { melee1, ranged1, ranged1 }, new int[] { 1, 3, 5 }));
+        }
+
+            //}
+		//}
 	}
 	
 	public void Wave1() {
